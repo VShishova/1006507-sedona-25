5cf9698103fa6d0610b7ade3318c6f89fa4851da
Vera Shishova <v.shish79@yandex.ru>
2019-06-08 23:04:18 +0300

    анимация

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var searchForm = document.querySelector(".search-form");
+var buttonVisible = document.querySelector(".button-form-visible");
+var dateIn = searchForm.querySelector("[name = date-out]");
+var dateOut = searchForm.querySelector("[name = date-in]");
+var adults = searchForm.querySelector("[name = adults]");
+
+searchForm.classList.add("form-hidden");
+
+buttonVisible.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchForm.classList.toggle("form-hidden");
+  if (searchForm.classList.contains("form-mistake")) {
+    searchForm.classList.remove("form-mistake");
+  }
+});
+
+searchForm.addEventListener("submit", function (evt) {
+  if (!dateIn.value || !dateOut.value || !adults.value) {
+    evt.preventDefault();
+    searchForm.classList.add("form-mistake");
+  }
+});
